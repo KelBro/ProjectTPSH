520e122d66de97ea38214df25ef52bf23cb01456
IlDottore1 <sonnadrimur125@gmail.com>
2025-03-27 10:31:09 +1000

    add func button for translate

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.utils.markdown import hbold, hitalic
@@ -12,112 +13,126 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher(storage=MemoryStorage())
 
+TRANSLATIONS = json.load(open("languages.json", encoding="utf-8"))
+def get_translations(lang):
+    return TRANSLATIONS[lang]
+tr = get_translations("ru")
+
 class DressStates(StatesGroup):
     waiting_for_name = State()
     waiting_for_photo = State()
 
-# Обработчик команды /start
+# пересоздание клавиатуры
+def get_keyboard(tr):
+    kb = [
+        [types.KeyboardButton(text=tr['upload'])],
+        [types.KeyboardButton(text=tr['history']),
+        types.KeyboardButton(text=tr['language'])]
+    ]
+    return types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True
+    )
+
+# обработчик команды /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    kb = [
-        [types.KeyboardButton(text="Загрузка")],
-        [types.KeyboardButton(text="История"),
-        types.KeyboardButton(text="Язык")]
-    ]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True # для более аккуратных кнопок
+    global tr
+    tr = get_translations("ru")
+    await message.answer(
+        f"{hbold(tr['greeting'])}"
+        f"{tr['description']}"
+        f"\n\n{hbold(tr['cta'])}",
+        reply_markup=get_keyboard(tr),
+        parse_mode="HTML"
     )
-    await message.answer(f"{hbold('Здравствуй! 👋')}"
-                         "\n\nЭто бот-ассистент по шоппингу! 🧥✨"
-                         "\nОтправь фото вещи — и бот отправит её детальную характеристику"
-                         f"\n\n{hbold('Попробуй сейчас — загрузи фото! 📸')}",
-                         reply_markup=keyboard,
-                         parse_mode="HTML")
 
-# Обработчик кнопки "Загрузка"
-@dp.message(F.text == "Загрузка")
+# обработчик кнопки загрузка
+@dp.message(lambda message: message.text in [lang['upload'] for lang in TRANSLATIONS.values()])
 async def handle_upload(message: types.Message, state: FSMContext):
     await state.set_state(DressStates.waiting_for_photo)
-    await message.answer("Отправьте фотографию вашей одежды")
+    await message.answer(tr['request_photo'])
 
-# Обработчик получения фотографии
+# обработчик получения фотографии
 @dp.message(F.photo, DressStates.waiting_for_photo)
 async def handle_photo(message: types.Message, state: FSMContext):
     buttons = [
         [
-            types.InlineKeyboardButton(text="Да", callback_data="ph_yes"),
-            types.InlineKeyboardButton(text="Нет", callback_data="ph_no")
+            types.InlineKeyboardButton(text=tr['yes'], callback_data="ph_yes"),
+            types.InlineKeyboardButton(text=tr['no'], callback_data="ph_no")
         ]
     ]
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
     await message.answer(
-        "Фото получено. Тут будет описание одежды\nНазвание:\n[цвет] платье [дата]\nИзменить?",
+        f"{tr['photo_received']}",
         reply_markup=keyboard
     )
     await state.clear()
 
-# Вывод ошибки для фото, отправленных без нажатия "Загрузка"
+# вывод ошибки для фото отправленных без нажатия "Загрузка"
 @dp.message(F.photo)
 async def handle_incorrect_photo(message: types.Message):
-    await message.answer("Нажмите кнопку 'Загрузка' чтобы загрузить фото")
+    await message.answer(tr['invalid_photo_input'])
 
-# Обработчик инлайн-кнопок подтверждения фото
+# обработчик инлайн-кнопок подтверждения фото
 @dp.callback_query(F.data.startswith("ph_"))
 async def handle_photo_callback(callback: types.CallbackQuery, state: FSMContext):
     action = callback.data.split("_")[1]
     if action == "yes":
-        await callback.message.answer("Введите новое название для платья:")
+        await callback.message.answer(tr['rename_prompt'])
         await state.set_state(DressStates.waiting_for_name)
     elif action == "no":
-        await callback.message.edit_text("Изменения отменены.\nТут будет описание одежды\nНазвание:\n[цвет] платье [дата]")
+        await callback.message.edit_text(tr['rename_cancel'])
     await callback.answer()
 
 # изменение названия платья
 @dp.message(DressStates.waiting_for_name)
 async def process_dress_name(message: types.Message, state: FSMContext):
     new_name = message.text
-    await message.answer(f"Имя платья успешно изменено на: {new_name}")
+    await message.answer(tr['rename_success']+new_name)
     await state.clear()
 
-# Обработчик кнопки "История"
-@dp.message(F.text == "История")
+# обработчик кнопки "История"
+@dp.message(lambda message: message.text in [lang['history'] for lang in TRANSLATIONS.values()])
 async def handle_history(message: types.Message):
     buttons = [
-        [types.InlineKeyboardButton(text="Синее платье 04.10", callback_data="num_finish")],
-        [types.InlineKeyboardButton(text="Зеленое платье 30.01", callback_data="num_finish")],
-        [types.InlineKeyboardButton(text="Красное платье 28.03", callback_data="num_finish")],
-        [types.InlineKeyboardButton(text="Тумба-юмба", callback_data="num_finish")],
-        [types.InlineKeyboardButton(text="Платье 1", callback_data="num_finish")],
+        [types.InlineKeyboardButton(text=tr['items'][0], callback_data="num_finish")],
+        [types.InlineKeyboardButton(text=tr['items'][1], callback_data="num_finish")],
+        [types.InlineKeyboardButton(text=tr['items'][2], callback_data="num_finish")],
+        [types.InlineKeyboardButton(text=tr['items'][3], callback_data="num_finish")],
+        [types.InlineKeyboardButton(text=tr['items'][4], callback_data="num_finish")],
         [
             types.InlineKeyboardButton(text="◀", callback_data="num_decr"),
             types.InlineKeyboardButton(text="▶", callback_data="num_incr")
         ]
     ]
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
-    await message.answer("История ваших запросов", reply_markup=keyboard)
+    await message.answer(tr['call_history'], reply_markup=keyboard)
 
-# Обработчик кнопки "Язык"
-@dp.message(F.text == "Язык")
+# обработчик кнопки "Язык"
+@dp.message(lambda message: message.text in [lang['language'] for lang in TRANSLATIONS.values()])
 async def handle_language(message: types.Message):
     buttons = [
-        [types.InlineKeyboardButton(text="Русский 🇷🇺", callback_data="lan_ru")],
-        [types.InlineKeyboardButton(text="Английский 🇬🇧", callback_data="lan_en")]
+        [types.InlineKeyboardButton(text=tr['options']['ru'], callback_data="lan_ru")],
+        [types.InlineKeyboardButton(text=tr['options']['en'], callback_data="lan_en")]
     ]
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
-    await message.answer("Выберите язык", reply_markup=keyboard)
+    await message.answer(tr['select'], reply_markup=keyboard)
 
-# Обработчик инлайн-кнопок языка
+# обработчик инлайн-кнопок языка
 @dp.callback_query(F.data.startswith("lan_"))
 async def handle_language_callback(callback: types.CallbackQuery):
+    global tr
     action = callback.data.split("_")[1]
-    if action == "ru":
-        await callback.message.edit_text("Теперь язык русский")
-    elif action == "en":
-        await callback.message.edit_text("Теперь язык английский")
+    tr = get_translations(action)
+    await callback.message.delete()
+    await callback.message.answer(
+        tr[f'changed_{action}'],
+        reply_markup=get_keyboard(tr)
+    )
     await callback.answer()
 
-# Обработчик команды /help
+# обработчик команды /help
 @dp.message(Command("help"))
 async def cmd_help(message: types.Message):
-    await message.answer("Это меню помощи! Тут будут инструкции")
+    await message.answer(tr['help'])
