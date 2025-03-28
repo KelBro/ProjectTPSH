7f474edc6970c87e937f3724f214951685b1a81e
ILiam <167431501+Blank12980@users.noreply.github.com>
2025-03-28 18:03:08 +1000

    Update train.py
    fix some bug

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pyarrow.parquet as pq
 from PIL import Image
 from io import BytesIO
-import os
+import os, shutil
 import random
 from ultralytics import YOLO
 import torch
@@ -12,8 +12,91 @@ from torch.optim import Adam
 class Train:
     def __init__(self):
 
-        self.lls = 12
+        self.lls = 0
+        self.pathToYaml = 'dataset'
+        # connect the dataset fro the parquet
+        # df = self.readTheParquet()
+        self.startTrain()
 
+        self.numList = ['a dress with color', 'department', 'detail', 'fabric-elasticity', 'fit', 'hemline', 'material', 'neckline', 'pattern', 'sleeve-length', 'style', 'type', 'waistline']
+        
+        #  get the converted dataset from the file.csv
+        # OBBdf = pd.read_csv('datas.csv')
+        # self.yoloCoving(df=OBBdf.values.tolist())
+
+        # df1 = df[:int(len(df)*0.7)]
+        # dfM = df[int(len(df)*0.7):]
+        # df2 = dfM[:int(len(dfM)*0.5)]
+        # df3 = dfM[int(len(dfM)*0.5):]
+        print('iis')
+        # self.useModel(modelPath='',imgPath='')
+
+
+        # newDf = self.saveDataset(df=self.DF)
+
+        # self.convert(type = 'train', df = df1, num=self.numList[self.lls])
+        # self.convert(type = 'test', df = df2, num=self.numList[self.lls])
+        # self.convert(type = 'val', df = df3, num=self.numList[self.lls])
+        # for i in self.numList:
+
+        #     print(self.getClasses(df=self.DF, num=i))
+
+    def useModel(self, modelPath:str, imgPath:str):
+        """use the created model """
+        model = YOLO('./best.pt')
+        img = './m.jpg'
+        result = model.predict(img)
+        print(result)
+    def yoloCoving(self,df:list):
+        """  """
+        print(df[0],'\n',df[1])
+        il =0
+
+        ir1 = int(len(df)*0.7)
+        ir2 = int((len(df)-ir1)*0.5)
+
+        df1 = df[:ir1]
+        df2 = df[ir1:] 
+        df2 = df2[:ir2]
+        df3 = df[ir1+ir2:]
+        path1 = './dataset/train/'
+        path2 = './dataset/test/'
+        path3 = './dataset/val/'
+
+        for i in df1:
+            try:
+                os.makedirs(path1+eval(i[2])[self.numList[self.lls]]+'/',exist_ok=True)
+            except:''
+            try:
+                shutil.copy(i[1], path1+eval(i[2])[self.numList[self.lls]]+'/')
+            except: ''
+
+        for i in df2:
+            try:
+                os.makedirs(path2+eval(i[2])[self.numList[self.lls]]+'/',exist_ok=True)
+            except:''
+            try:
+                shutil.copy(i[1], path2+eval(i[2])[self.numList[self.lls]]+'/')
+            except: ''
+        for i in df3:
+    
+            try:
+                os.makedirs(path3+eval(i[2])[self.numList[self.lls]]+'/',exist_ok=True)
+            except:''
+            try:   
+                shutil.copy(i[1], path3+eval(i[2])[self.numList[self.lls]]+'/')
+            except: ''
+
+        self.createYAML()
+        # for i in df:
+        #     try:
+        #        print(i[1], eval(i[2])[self.numList[0]])
+        #     except Exception as e: 
+        #         print(f'error is {e}')
+        #         il+=1
+        # print(il)
+    def readTheParquet(self):
+        """ read the dataset from the parquets files """
         # load the dataset and devide to 4:1
         dataPath = './data/'
         self.pathToYaml = ''
@@ -24,7 +107,6 @@ class Train:
         df5 = pq.read_table(f'{dataPath}dataset5.parquet').to_pandas()
 
         df = pd.concat([df1,df2,df3,df4,df5]).values.tolist()
-        
         for i in df:
             cef = {}
             for f in f'{i[1]}\n'.split(','):
@@ -33,26 +115,13 @@ class Train:
             i[1] = cef
         # print(str(df[0][1]), len(df), len(df[0][1]))
         self.DF = df
-        self.numList = ['a dress with color', 'department', 'detail', 'fabric-elasticity', 'fit', 'hemline', 'material', 'neckline', 'pattern', 'sleeve-length', 'style', 'type', 'waistline']
+
+        # shuffle the datas
         random.shuffle(df)
-        df1 = df[:int(len(df)*0.7)]
-        dfM = df[int(len(df)*0.7):]
-        df2 = dfM[:int(len(dfM)*0.5)]
-        df3 = dfM[int(len(dfM)*0.5):]
 
-        print(str(df[0][0])[:100])
-        newDf = self.saveDataset(df=self.DF)
-
-        
-
-        # self.convert(type = 'train', df = df1, num=self.numList[self.lls])
-        # self.convert(type = 'test', df = df2, num=self.numList[self.lls])
-        # self.convert(type = 'val', df = df3, num=self.numList[self.lls])
-        # for i in self.numList:
-
-        #     print(self.getClasses(df=self.DF, num=i))
+        return df
+    
     def convert(self, df:list, type:str, num:str): # part of dataframe and the type for exmp: train
-
         """ convert dataframe to dataframe for YOLO V8"""
         for i in range(len(df)):
             img = df[i][0]['bytes']
@@ -69,10 +138,10 @@ class Train:
         """ save the dataset to usb may be """
         count = 0
         for i in df:
-            # img = i[0]['bytes']
-            # img = BytesIO(img)
-            # img = Image.open(img)
-            # img.save(f'./image/image{count}.png')
+            img = i[0]['bytes']
+            img = BytesIO(img)
+            img = Image.open(img)
+            img.save(f'./image/image{count}.png')
             i[0] = (f'./image/image{count}.png')
             count+=1
         df = pd.DataFrame(df,columns = ['path', 'text'])
@@ -93,24 +162,28 @@ class Train:
         # if count!=0: print(count)
         return cl
     
-    def createYAML(self, classes:list):
+    def createYAML(self):
         """ ADD the yaml file to dataset for yolo """
-        ls = self.getClasses(self.DF)
-        with open(f'dataset{self.lls}/', 'w', encoding='utf-8') as file:
-            file.write(f"""
-        train: dataset{self.lls}/train/
-        valid: dataset{self.lls}/val/
-        test: dataset{self.lls}/test/
+        ls = self.getClasses(self.DF, self.numList[self.lls])
+        with open(f'dataset/file.yaml', 'w', encoding='utf-8') as file:
+            file.write(f"""\
+train: dataset/train/
+valid: dataset/val/
+test: dataset/test/
 
-        # number of classes
-        nc: {len(ls)}
+# number of classes
+nc: {len(ls)}
 
-        # class names
-        names: {str(ls)}
+# class names
+names: {str(ls)}
         """)
     
 
-    def train(self,num:str, data, model, epoch:int, ilr = 1e-4):
+        
+
+        
+
+    def train(self, data, model, epoch:int, ilr = 1e-4):
         """ train the model """
 
         # freezing
@@ -138,6 +211,9 @@ class Train:
         model = YOLO('yolov8n-cls.pt')
         trData = self.pathToYaml
         self.train(model=model, data=trData, epoch=5,ilr=1e-4)
+
+
+Train()
 
 
 Train()
